02942bae1e6540c55cb6a9b0c3cbda0742bd0a5c
xXSamXx00 <samudichio@gmail.com>
2021-12-17 17:32:01 +0100

    Footer complete with a separate component

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,77 @@
+<footer id="site_footer">
+        <div class="link_footer">
+            <div class="container">
+                <div class="list_links">
+                    <div class="list">
+                        <h3>Dc comics</h3>
+                        <ul>
+                            <li><a href="#">Characters</a></li>
+                            <li><a href="#">Comics</a></li>
+                            <li><a href="#">Movies</a></li>
+                            <li><a href="#">Tv</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Videos</a></li>
+                            <li><a href="#">News</a></li>
+                        </ul>
+                        <h3>Shop</h3>
+                        <ul>
+                            <li><a href="#">Shop DC</a></li>
+                            <li><a href="#">Shop DC Collectibles</a></li>
+                        </ul>
+                    </div>
+                    <div class="list">
+                        <h3>DC</h3>
+                        <ul>
+                            <li><a href="#">Terms Of Use</a></li>
+                            <li><a href="#">Privacy policy (New)</a></li>
+                            <li><a href="#">Ad Choices</a></li>
+                            <li><a href="#">Advertising</a></li>
+                            <li><a href="#">Jobs</a></li>
+                            <li><a href="#">Subscriptions</a></li>
+                            <li><a href="#">Talent Workshops</a></li>
+                            <li><a href="#">CPSC Certificates</a></li>
+                            <li><a href="#">Ratings</a></li>
+                            <li><a href="#">Shop Help</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                        </ul>
+                    </div>
+                    <div class="list">
+                        <h3>Sites</h3>
+                        <ul>
+                            <li><a href="#">DC</a></li>
+                            <li><a href="#">MAD Magazine</a></li>
+                            <li><a href="#">DC Kids</a></li>
+                            <li><a href="#">DC Universe</a></li>
+                            <li><a href="#">DC Power Visa</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <p>All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.<br><a href="#">Cookies Settings</a></p>
+            </div>
+        </div>
+        <div class="contact">
+            <div class="container">
+                <div class="button">
+                    <button>Sign-up now&excl;</button>
+                </div>
+                <div class="socials">
+                    <h3>Follow Us</h3>
+                    <div class="social">
+                        <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+                    </div>
+                    <div class="social">
+                        <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+                    </div>
+                    <div class="social">
+                        <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+                    </div>
+                    <div class="social">
+                        <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+                    </div>
+                    <div class="social">
+                        <img src="{{ asset('img/footer-periscope.png') }}" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
