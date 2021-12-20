5c44ea320f5d2f903c7dba5e53735fc1b93e8838
xXSamXx00 <samudichio@gmail.com>
2021-12-20 17:16:44 +0100

    Main Css coplete with dynamic structure

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -1,12 +1,40 @@
 @extends('layouts.app')
 
-@section('page-title', 'Comics')
+@section('page-title', 'Comic')
 
 @section('content')
 <div class="single_comic">
-    <div class="card_comics">
-        <img src="{{ $comic['thumb'] }}" alt="Comics">
-        <h3>{{ $comic['series'] }}</h3>
+    <div class="blue_bar position-relative">
+        <div class="container px_container">
+            <div class="card_comic position-absolute">
+                <img class="position-relative" src="{{ $comic['thumb'] }}" alt="Comics">
+                <div class="text">
+                    <p class="position-absolute">Comic Book</p>
+                    <p class="position-absolute">View Gallery</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="info_comic">
+        <div class="container px_container">
+            <div class="row">
+                <div class="details col-8">
+                    <h2>{{ $comic['title'] }}</h2>
+                    <div class="price">
+                        <div class="available">
+                            <p>U.S. Price: <span>{{ $comic['price'] }}</span></p>
+                            <p>Available</p>
+                        </div>
+                        <button>Check Availability &dtrif;</button>
+                    </div>
+                    <p class="description">{{ $comic['description'] }}</p>
+                </div>
+                <div class="advertisement col-4 d-flex flex-column align-items-end">
+                    <h3>Advertisement</h3>
+                    <img src="{{ asset('img/image.jpg') }}" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
